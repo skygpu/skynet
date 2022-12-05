72149ae1b4da630236dd92bacb396b30ca34a1eb
Guillermo Rodriguez <guillermo@telos.net>
2022-12-05 19:36:21 -0300

    Add roles and start working on algo selection, clean up rr code

--- run-bot.sh
+++ run-bot.sh
@@ -4,7 +4,7 @@ docker run \
     --gpus=all \
     --env HF_TOKEN='' \
     --env DB_USER='skynet' \
-    --env DB_PASS='password' \
+    --env DB_PASS='nnf01nmf091d0i' \
     --mount type=bind,source="$(pwd)"/outputs,target=/outputs \
     --mount type=bind,source="$(pwd)"/hf_home,target=/hf_home \
     --mount type=bind,source="$(pwd)"/scripts,target=/scripts \
