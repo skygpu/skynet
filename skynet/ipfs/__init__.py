469e90e65027a9102e280bc4261fb3c6d4123c07
Konstantine Tsafatinos <kosta.tsaf@gmail.com>
2023-07-21 18:57:28 -0400

    change back the interval to 60, and double ipfs interval

--- skynet/ipfs/__init__.py
+++ skynet/ipfs/__init__.py
@@ -27,7 +27,7 @@ class IPFSHTTP:
 async def get_ipfs_file(ipfs_link: str):
     logging.info(f'attempting to get image at {ipfs_link}')
     resp = None
-    for i in range(10):
+    for i in range(20):
         try:
             resp = await asks.get(ipfs_link, timeout=3)
 
