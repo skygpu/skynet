1b13cf25cce6ea3d1ca8977ae0bcf25aefe3358e
Guillermo Rodriguez <guillermo@telos.net>
2023-09-24 15:23:25 -0300

    Add test for new ipfs async apis, fix cli entrypoints endpoint loading to new format

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,14 +1,17 @@
 #!/usr/bin/python
 
-import logging
-
-from pathlib import Path
-
 import pytest
 
 from skynet.db import open_new_database
+from skynet.ipfs import AsyncIPFSHTTP
+from skynet.ipfs.docker import open_ipfs_node
 from skynet.nodeos import open_nodeos
 
+
+@pytest.fixture(scope='session')
+def ipfs_client():
+    with open_ipfs_node(teardown=True):
+        yield AsyncIPFSHTTP('http://127.0.0.1:5001')
 
 @pytest.fixture(scope='session')
 def postgres_db():
