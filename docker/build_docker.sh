e88792c9d6ffd831c3ccf4704587e9c40e8e23df
Guillermo Rodriguez <guillermo@telos.net>
2024-11-02 14:52:22 -0300

    Fix docker paths

--- docker/build_docker.sh
+++ docker/build_docker.sh
@@ -1,20 +1,20 @@
 
 docker build \
     -t guilledk/skynet:runtime \
-    -f Dockerfile.runtime .
+    -f docker/Dockerfile.runtime .
 
 docker build \
     -t guilledk/skynet:runtime-frontend \
-    -f Dockerfile.runtime+frontend .
+    -f docker/Dockerfile.runtime+frontend .
 
 docker build \
     -t guilledk/skynet:runtime-cuda-py311 \
-    -f Dockerfile.runtime+cuda-py311 .
+    -f docker/Dockerfile.runtime+cuda-py311 .
 
 docker build \
     -t guilledk/skynet:runtime-cuda \
-    -f Dockerfile.runtime+cuda-py311 .
+    -f docker/Dockerfile.runtime+cuda-py311 .
 
 docker build \
     -t guilledk/skynet:runtime-cuda-py310 \
-    -f Dockerfile.runtime+cuda-py310 .
+    -f docker/Dockerfile.runtime+cuda-py310 .
