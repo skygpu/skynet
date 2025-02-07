d8de028814351c52b1fbc26448fac1e419892ac8
Guillermo Rodriguez <guillermo@telos.net>
2025-02-07 15:07:48 -0300

    Create msgspec struct for config

--- skynet/config.py
+++ skynet/config.py
@@ -1,27 +1,70 @@
 import os
 import toml
 
-from .constants import DEFAULT_CONFIG_PATH
+import msgspec
+
+from skynet.constants import DEFAULT_CONFIG_PATH, DEFAULT_IPFS_DOMAIN
 
 
 class ConfigParsingError(BaseException):
     ...
 
 
-def load_skynet_toml(file_path=DEFAULT_CONFIG_PATH) -> dict:
-    config = toml.load(file_path)
-    return config
+class DgpuConfig(msgspec.Struct):
+    account: str
+    permission: str
+    key: str
+    node_url: str
+    hyperion_url: str
+    ipfs_url: str
+    hf_token: str
+    ipfs_domain: str = DEFAULT_IPFS_DOMAIN
+    hf_home: str = 'hf_home'
+    non_compete: set[str] = set()
+    model_whitelist: set[str] = set() 
+    model_blacklist: set[str] = set() 
+    backend: str = 'sync-on-thread'
+    api_bind: str = False
+    tui: bool = False
 
+class TelegramConfig(msgspec.Struct):
+    account: str
+    permission: str
+    key: str
+    node_url: str
+    hyperion_url: str
+    ipfs_url: str
+    token: str
 
-def load_key(config: dict, key: str) -> str:
-    for skey in key.split('.'):
-        if skey not in config:
-            conf_keys = [k for k in config]
-            raise ConfigParsingError(f'key \"{skey}\" not in {conf_keys}')
+class DiscordConfig(msgspec.Struct):
+    account: str
+    permission: str
+    key: str
+    node_url: str
+    hyperion_url: str
+    ipfs_url: str
+    token: str
 
-        config = config[skey]
+class PinnerConfig(msgspec.Struct):
+    hyperion_url: str
+    ipfs_url: str
 
-    return config
+class UserConfig(msgspec.Struct):
+    account: str
+    permission: str
+    key: str
+    node_url: str
+
+class Config(msgspec.Struct):
+    dgpu: DgpuConfig | None = None
+    telegram: TelegramConfig | None = None
+    discord: DiscordConfig | None = None
+    pinner: PinnerConfig | None = None
+    user: UserConfig | None = None
+
+def load_skynet_toml(file_path=DEFAULT_CONFIG_PATH) -> Config:
+    with open(file_path, 'r') as file:
+        return msgspec.toml.decode(file.read(), type=Config)
 
 
 def set_hf_vars(hf_token: str, hf_home: str):
